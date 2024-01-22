75b56e952d053f3285f8b7387bd3c798ca6b46b5
NicoloCammarata <119122715+NicoloCammarata@users.noreply.github.com>
2024-01-22 15:11:09 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script >
+import appHeader from './components/appHeader.vue';
+import appMain from './components/appMain.vue';
+import appFooter from './components/appFooter.vue';
+
+
+export default {
+    data() {
+        return 
+    },
+    methods:{
+
+    },
+    components: {
+        appHeader,
+        appMain,
+        appFooter,
+
+    }
+  
+}
+</script>
+
+<template>
+  
+  <appHeader />
+
+  <appMain />
+
+  <appFooter />
+  
+</template>
+
+<style lang="scss" >
+@use "assets/scss/main" as *;
+@import "assets/scss/partials/reset";
+
+</style>
---
+++ src/components/appHeader.vue
@@ -0,0 +1,91 @@
+<script >
+export default {
+    data() {
+        return {
+            headerMenu: [
+                {
+                    label: 'Caracters',
+                    link: '#',
+                    active: false,
+                },
+                {
+                    label: 'Comics',
+                    link: '#',
+                    active: true,
+                },
+                {
+                    label: 'Movies',
+                    link: '#',
+                    active: false,
+                },
+                {
+                    label: 'Tv',
+                    link: '#',
+                    active: false,
+                },
+                {
+                    label: 'Games',
+                    link: '#',
+                    active: false,
+                },
+                {
+                    label: 'Collectibles',
+                    link: '#',
+                    active: false,
+                },
+                {
+                    label: 'Videos',
+                    link: '#',
+                    active: false,
+                },
+                {
+                    label: 'News',
+                    link: '#',
+                    active: false,
+                },
+                {
+                    label: 'Shop',
+                    link: '#',
+                    active: false,
+                },
+            ]
+
+        }
+    },
+    methods:{
+        
+
+    },
+}
+
+</script>
+
+<template>
+    <header>
+        <div class="top-header">
+
+        </div>
+        <div class="bottom-header">
+            <div class="logo">
+                <img src="../../public/img/dc-logo.png" alt="">
+
+            </div>
+            <ul class="link">
+                <li @click="menu.active = true" :class="{
+                    'active':menu.active
+                }" v-for="(menu, i) in headerMenu">
+                    <a :href="menu.link">{{ menu.label }}</a>
+
+                </li>
+
+            </ul>
+
+        </div>
+   </header>
+  
+</template>
+
+<style lang="scss" scoped>
+@use "../assets/scss/partials/reset" as *;
+
+</style>
